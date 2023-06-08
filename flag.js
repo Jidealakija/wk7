3457bb2dc13a41b191041897f613295caf61cde1
jidealakija <francisalakija@gmail.com>
2023-06-08 16:16:24 +0100

    feat: first javascript class

---
+++ flag.js
@@ -0,0 +1,33 @@
+// let myName = 'jide'
+
+// myName = 'jide'
+
+// const school = 'univelcity'
+
+// const state = `Lagos`
+
+// const statement = `my name is ${myName} and i study at ${school} and i am from ${state}`
+
+// const statement2 = "Mohammed Ali said 'suffer now and live the rest of your life a champion'"
+
+// let age = prompt('how old are you')
+
+// console.log(`you were born in ${new Date().getFullYear() - age}`)
+
+// console.log(statement2)
+
+// // alert(statement2)
+
+// // confirm(statement2)
+
+
+
+let fry = prompt('what is your name')
+
+let age = prompt('what year were you born?')
+
+let statement3 = `Your name is ${fry} and
+ you are ${new Date().getFullYear() - age} years
+  old`
+
+console.log(statement3)
